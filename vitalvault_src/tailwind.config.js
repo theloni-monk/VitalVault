06348e927e1fcb94a6de09846c96ce1dcabc54ac
theloni-monk <theo.acooper@gmail.com>
2025-06-11 17:01:31 -0700

    Ian told me how to fix stupid css linker bug: off-by-one spacing is a bastard and a half

--- vitalvault_src/tailwind.config.js
+++ vitalvault_src/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./components/*.{ts, tsx}","./app/*.{ts, tsx}","./app/**/*.{ts, tsx}"],
+  content: ["./components/*.{ts,tsx}",
+            "./app/*.{ts,tsx}",
+            "./app/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
