1610a8026b45800f172d46f1427564bf6bca26fd
theloni-monk <theo.acooper@gmail.com>
2026-02-19 22:48:54 -0800

    minimum viable building backend

--- vitalvault_src/src-tauri/src/lib.rs
+++ vitalvault_src/src-tauri/src/lib.rs
@@ -1,30 +1,53 @@
-use std::Option;
+#![allow(unused)] // Disable dead code warnings for the entire crate
+ 
+use std::option::Option;
 use std::sync::Mutex;
-
+use duckdb::{params, Connection, Result};
 use tauri::{Builder, Manager};
 
-use docstuff;
+use crate::docstruct::DocScope;
 
-struct MockDBConn{} //TODO: replace with DuckDB connection type
-struct MockMLEngine{} //TODO: replace Burn Backend reference
+mod docstruct;
+mod vvbackend;
+mod mlpipeline;
 
-struct AppState{
-  Option<MockDBConn> db
-  bool has_auth = false
-  Option<DocScope> scope
-  Option<MockMLEngine> mlengine
+struct MockMLEngine{} //TODO: replace Burn runtime Backend reference
+struct MockAuthToken{} //TODO: replace with Stronghold auth token from native keychain via tauri-biometry
+struct BackendState{
+  db : Connection,
+  auth_token: MockAuthToken,
+  activescope : DocScope,
+  mlengine: Option<MockMLEngine> 
 }
+
+//TODO: write tests
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   Builder::default()
     .setup(|app| {
       if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
+        #[cfg(mobile)]
+        app.handle().plugin(tauri_plugin_biometry::init());
+
+        let salt_path = app
+                .path()
+                .app_local_data_dir()
+                .expect("could not resolve app local data path")
+                .join("vv.salt");
+  
+        app.handle()
+          .plugin(tauri_plugin_stronghold::Builder::with_argon2(&salt_path).build())?;
+
+        app.handle()
+          .plugin(
+            tauri_plugin_log::Builder::default()
+              .level(log::LevelFilter::Info)
+              .build()
+          )?;
+
+        app.handle()
+          .plugin(tauri_plugin_fs::init())?;
       }
       Ok(())
     })
--- vitalvault_src/src-tauri/src/mlpipeline.rs
+++ vitalvault_src/src-tauri/src/mlpipeline.rs
@@ -1,39 +1,85 @@
-use std::Cursor;
-use std::sync::Mutex;
-use Uuid::Uuid;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::BufReader;
+use std::{io::Cursor, str::Bytes};
+use uuid::Uuid;
+use async_trait::async_trait;
 
-enum JobType{
-    LinearizePDF, // PDF text extraction
-    SemanticEmbed, // minish potion multlingual mebdding
-    ImgOCR // OCR
+use crate::docstruct::{DocID, SemanticHash};
+
+#[derive(Debug, Serialize, Deserialize)]
+enum JobType {
+    LinearizePDF,  // PDF text extraction
+    SemanticEmbed, // minish potion multlingual embedding
+    ImgOCR         // OCR
 }
 
-// Task that is written to disk on creation, cleared on completion
+#[derive(Debug, Serialize, Deserialize)]
+struct TaskContext {}
+
+// TODO: task necromancy
+type TaskID = Uuid;
 // goal is to be able to resume(or at least restart) incomplete jobs on boot
-struct PersistantTask {
-    Uuid doc_id,
-    JobType jobtype,
-    Mutex<bool> needs_data,
-    Mutex<bool> in_flight
+#[derive(Debug, Serialize, Deserialize)]
+struct Task {
+    target_id: DocID,                   // document to exectute on
+    task_id: TaskID,                    // for necromancy
+    priority: u8,
+
+    jobtype: JobType,
+    resumable: bool,
+    interrupted: bool,
+
+    launchtime: std::time::SystemTime,  //TODO: handle date-time w chronos
+    ctx: TaskContext,                   //ModelContext allows for resuming
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+enum TaskError {
+    TargetIOFailure,
+    ModelFailure,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TaskUpdate {
+    task_id: TaskID,
+    killed: bool,
+    ctx: Result<TaskContext, TaskError>,
+}
+
+// when you build a taskqueue, it will populate with persisted jobs from disk
 struct TaskQueue {} // alias for priorityq of mltasks
+
+// far-off todo make a macro to derive this
+// I want to wrap every function call that takes a &mut self on some object with a given persistence hook
+trait DiskPersistant<T> {} // trait for a struct that is written to disk upon update or creation
+
 struct Backend {} // replace with burn backend
-struct MLModel {
-    bool loaded = false,
-    Cursor parambuff
+
+#[derive(Debug, Serialize, Deserialize)]
+enum ModelError {
+    BadInput,
+    BadParams,
+    InvalidCTX,
 }
 
-struct MLEngine {
-    Backend backend, // runtime reference
-    MLModel pdfmodel,
-    MLModel imgmodel,
-    MLModel txtmodel,
-    TaskQueue jobs
+#[async_trait]
+trait AIModel<I, O> {
+    async fn load(&mut self, param_buff: &BufReader<Bytes>) -> bool;
+    async fn process(&self, data_in: I) -> Result<O, ModelError>;
 }
 
-Impl MLEngine {
-    fn new(&mut self) -> Result<MLEngine>{
+struct MLEngine<'a> {
+    backend: Backend, // owned runtime
+    pdfocr: &'a dyn AIModel<File, String>,
+    imgmodel: &'a dyn AIModel<File, crate::docstruct::SemanticHash>,
+    txtmodel: &'a dyn AIModel<String, crate::docstruct::SemanticHash>,
+    jobs: TaskQueue,
+}
 
+impl MLEngine<'_> {
+    // TODO: scheme for model serialization and loading from disk
+    fn new(&mut self, lazy_models: bool) -> Result<MLEngine<'_>, ModelError> {
+        Err(ModelError::BadParams)
     }
 }
